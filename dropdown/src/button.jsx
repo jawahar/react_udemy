7c260899d38bef16c5b137a064902c2476d5b474
Jawahar Malhotra <jawahar.malhotra@gmail.com>
2016-01-02 16:34:43 -0800

    commit after lesson 19

--- dropdown/src/button.jsx
+++ dropdown/src/button.jsx
@@ -2,7 +2,7 @@ var react = require('react');
 
 module.exports = React.createClass({
   render: function () {
-    return <button className={"btn " + this.props.className} type="button">
+    return <button onClick={this.props.whenClicked} ClclassName={"btn " + this.props.className} type="button">
         {this.props.title}
         <span className={this.props.subTitleClassName}>
           {this.props.subTitle}
--- dropdown/src/dropdown.jsx
+++ dropdown/src/dropdown.jsx
@@ -2,9 +2,15 @@ var React = require('react');
 var Button = require('./button.jsx');
 
 module.exports = React.createClass({
+
+  handleClick: function() {
+    alert("click in dropdown");
+  },
+
   render: function() {
     return <div className="dropdown">
-        <Button className="btn-default" title={this.props.title} subTitleClassName="caret" />
+        <Button whenClicked={this.handleClick} className="btn-default" title={this.props.title} subTitleClassName="caret" />
       </div>
   }
+
 });
